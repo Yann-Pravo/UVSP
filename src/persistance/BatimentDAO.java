59c659a8bafe7a32a427ee4450f40054bd897445
Clément Balestrat <clementbalestrat@polytech-116-17.pub.polytech.univ-montp2.fr>
2012-03-05 17:16:06 +0100

    add persistance

---
+++ src/persistance/BatimentDAO.java
@@ -0,0 +1,44 @@
+package persistance;
+import metier.*;
+
+import java.util.ArrayList;
+
+public class BatimentDAO extends DAO<Batiment>{
+
+	@Override
+	public boolean create(Batiment obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Batiment obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Batiment ancien, Batiment nouveau) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Batiment find(Batiment obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(Batiment obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<Batiment> getListe() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
