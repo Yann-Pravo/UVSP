5d462dd162a82776ae6a9a0049e3305e0230e70c
Clément Balestrat <clementbalestrat@polytech-116-17.pub.polytech.univ-montp2.fr>
2012-03-08 10:46:19 +0100

    ajout test.java

--- src/test.java
+++ src/test.java
@@ -1,12 +1,24 @@
 import metier.*;
+import jdbc.*;
 import persistance.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class test 
 {
 	public static void main(String [] args)
 	{
 		
+		DAO<Enseignant> ens = AbstractDAOFactory.getFactory(AbstractDAOFactory.DAO_FACTORY).getEnseignantDAO();
+		Enseignant e = new Enseignant(1);
 		
+		e = ens.find(e);
 		
 		
 		
